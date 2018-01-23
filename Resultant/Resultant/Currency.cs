e9df522e0fa4f60eeb93b01fc108dc98b855e346
Eduard <wolfed11.4@gmail.com>
2018-01-24 05:02:24 +0700

    Тестовое задание:
    1. Разработать launch screen (отобразить любую иконку 64x64 в середине экрана)
    
    2. Разработать Главный экран - представляет собой таблицу - список валют. Список валют нужно получать по ссылке: http://phisix-api3.appspot.com/stocks.json
    
    3. Ячейка валюты - название валюты, цена и количество. Название валюты - поле "name", цена - поле "volume", количество - поле "amount". Volume отображается как целое число. Amount - 2 знака после запятой. Высота ячейки - 50.
    
    4. Данные в таблице должны обновляться каждые 15 секунд.
    
    5. Также должна быть кнопка в ручного обновления данных в навигационном меню справа.

---
+++ Resultant/Resultant/Currency.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resultant
+{
+    //Класс Цена
+    public class Price
+    {
+        public string currency { get; set; }
+        public double amount { get; set; }
+    }
+
+    //Класс Валюта
+    public class Stock
+    {
+        public string name { get; set; }
+        public Price price { get; set; }
+        public double percent_change { get; set; }
+        public int volume { get; set; }
+        public string symbol { get; set; }
+    }
+
+    public class StockForVision
+    {
+        public string name { get; set; }
+        public int volume { get; set; }
+
+        public double amount { get; set; }
+    }
+
+    public class Stocks
+    {
+        public List<Stock> stock { get; set; }
+        public DateTime as_of { get; set; }
+    }
+}
---
+++ Resultant/Resultant/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading;
+
+namespace Resultant
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Ссылка на JSON
+        private const string linkJSON = "http://phisix-api3.appspot.com/stocks.json";
+
+        // Сток валюты
+        private Stocks StocksCurrency = new Stocks();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            TimeSpan interval = new TimeSpan(0, 0, 15);
+            CancellationToken ct = new CancellationToken();
+            RunPeriodicallyAsync(TakeInfo, interval, ct);
+             
+        }
+
+        //Подпрограмма, вызывающаяся каждые 15 сек.
+        private async Task RunPeriodicallyAsync(Func<Task> func,  TimeSpan interval, CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                await func();
+                await Task.Delay(interval, cancellationToken);
+            }
+        }
+
+        private async Task TakeInfo()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var requestMessage = new HttpRequestMessage
+                {
+                    RequestUri = new Uri(linkJSON),
+                    Method = HttpMethod.Get
+                };
+                var response = await httpClient.SendAsync(requestMessage);
+
+                var result = response.Content.ReadAsStringAsync().Result;
+                StocksCurrency = JsonConvert.DeserializeObject<Stocks>(result);
+                textBlockToDateTime.Text = "Данные верны на: " + StocksCurrency.as_of.ToLocalTime();
+                dataGrid.Items.Clear();
+                foreach(Stock item in StocksCurrency.stock)
+                {
+                    StockForVision tempSt = new StockForVision();
+                    tempSt.name = item.name;
+                    tempSt.amount = Math.Round(item.price.amount,2);
+                    tempSt.volume = item.volume;
+                    dataGrid.Items.Add(tempSt);
+                }
+            }
+                    
+        }
+
+        async private void buttonRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            await TakeInfo();
+        }
+    }
+}
